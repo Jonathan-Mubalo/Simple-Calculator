c5bd362064e3404225756e24006bf0d8588c2f30
Jonathan-Mubalo <mubalokalombejonathan@gmail.com>
2026-05-12 09:56:58 +0200

    My butttons for addition works without using evaluate

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const calculatorDisplay= document.getElementById('display');
 
 function calculatorOperator(input){
-    calculatorDisplay['value'] += input;
+  return  calculatorDisplay['value'] += input;
 }
 
 function clearDisplay(){
-    calculatorDisplay.value = '';
+   return calculatorDisplay.value = '';
 }
  
 function calculate(){
-    calculatorDisplay.value= eval(calculatorDisplay.value);
+   if(calculatorDisplay.value.includes("+") == true){
+      let plusPos= calculatorDisplay.value.indexOf("+");
+      let firstPart= calculatorDisplay.value.slice(0,plusPos);
+      let firstValue= parseFloat(firstPart);
+      let secondPart= calculatorDisplay.value.slice(plusPos+1);
+      let secondValue= parseFloat(secondPart);
+
+      let finalAnswer= firstValue + secondValue;
+     return calculatorDisplay.value = finalAnswer;}
+   
+   //return calculatorDisplay.value= eval(calculatorDisplay.value);
 }
+
+
